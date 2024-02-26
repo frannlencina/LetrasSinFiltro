08ce9117d96cf4d2005802d409c66d03283372b7
Francisco <cinkii@protonmail.com>
2024-02-26 17:39:50 -0300

    ⚡️ cors error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "http://letrassinfiltro.site/" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+}
 
 module.exports = {
   env: {
@@ -10,5 +24,6 @@ module.exports = {
     POST_URL_LOGIN: process.env.POST_URL_LOGIN,
     POST_URL_REGISTER: process.env.POST_URL_REGISTER
   },
+  ...nextConfig
 }
 
