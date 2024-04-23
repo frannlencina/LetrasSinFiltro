8cff6281885550accc48687ff015bdc74e93be30
Francisco <cinkii@protonmail.com>
2024-04-23 16:21:42 -0300

    feature: update the badge component to dynamic component ✨

--- src/app/components/Badge.tsx
+++ src/app/components/Badge.tsx
@@ -1,20 +1,60 @@
 interface BadgeProps {
     text: string;
     icon: boolean;
-    type: string; // Add the type property
+    type: string;
+}
+
+interface BadgeType {
+    style: string;
+    icon: string;
+}
+
+interface Types {
+    [key: string]: BadgeType;
+}
+
+const types: Types = {
+    defaultStyle: {
+        style: ' bg-opacity-50 px-3 py-1 rounded-md text-xs font-medium ',
+        icon: ''
+    },
+    succes: {
+        style: '',
+        icon: ''
+    },
+    information: {
+        style: ' bg-blue-200 text-blue-600 ',
+        icon: 'info-i'
+    },
+    warning: {
+        style: ' bg-yellow-200 text-yellow-600 ',
+        icon: 'error-warning-line'
+    },
+    attention: {
+        style: '',
+        icon: ''
+    },
+    error: {
+        style: ' bg-red-200 text-red-600 ',
+        icon: 'arrow-up-circle-line'
+    },
 }
 
 const Badge: React.FC<BadgeProps> = ({ text, type, icon }) => {
+
+    const { defaultStyle } = types;
+    const selectedType = types[type] || {};
+
     return (
         <div>
-            {
-                icon ? <span className="bg-yellow-200 bg-opacity-50 text-yellow-600 px-3 py-1 rounded-md text-xs font-medium flex items-center gap-3 select-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-alert-square-rounded-filled" width="20" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M12 2l.642 .005l.616 .017l.299 .013l.579 .034l.553 .046c4.687 .455 6.65 2.333 7.166 6.906l.03 .29l.046 .553l.041 .727l.006 .15l.017 .617l.005 .642l-.005 .642l-.017 .616l-.013 .299l-.034 .579l-.046 .553c-.455 4.687 -2.333 6.65 -6.906 7.166l-.29 .03l-.553 .046l-.727 .041l-.15 .006l-.617 .017l-.642 .005l-.642 -.005l-.616 -.017l-.299 -.013l-.579 -.034l-.553 -.046c-4.687 -.455 -6.65 -2.333 -7.166 -6.906l-.03 -.29l-.046 -.553l-.041 -.727l-.006 -.15l-.017 -.617l-.004 -.318v-.648l.004 -.318l.017 -.616l.013 -.299l.034 -.579l.046 -.553c.455 -4.687 2.333 -6.65 6.906 -7.166l.29 -.03l.553 -.046l.727 -.041l.15 -.006l.617 -.017c.21 -.003 .424 -.005 .642 -.005zm.01 13l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z" strokeWidth="0" fill="currentColor"></path>
-                    </svg> {text}</span>
-                    : <span className="bg-yellow-200 bg-opacity-50 text-yellow-600 px-3 py-1 rounded-md text-xs font-medium">{text}</span>
-            }
+           {icon ? (
+                <span className={`${defaultStyle.style} ${selectedType.style}`}>
+                    <i className={`ri-${selectedType.icon}`}></i>
+                    {text}
+                </span>
+            ) : (
+                <span className={`${defaultStyle.style} ${selectedType.style}`}>{text}</span>
+            )}
         </div>
     );
 };
